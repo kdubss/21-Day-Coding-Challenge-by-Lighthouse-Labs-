db99cec8e31194b5c6639e31cfb9bcd34dd1b1a7
Kang Wang <kwang.inbox@gmail.com>
2018-10-10 09:37:51 -0700

    Day 10 challenge - lightRow() function

---
+++ day10.js
@@ -0,0 +1,30 @@
+// .day10.js
+
+// ## Challenge #10
+// Your next job is to write a function so that your lighthouse can
+// sweep a whole row of cells.
+
+// Write a function named lightRow() that takes in the number of the row
+// and returns its contents. (Example: lightRow(2); would return ["",
+// "", "v", "", "~", "", "", "", "", ""])
+
+const GRID = [
+      ["", "", "", "^", "", "", "", "", "", ""],
+      ["", "", "v", "", "~", "", "", "", "", ""],
+      ["", "v", "", "", "^", "^", "", "", "", ""],
+      ["", "", "", "", "^", "^", "", "", "", ""],
+      ["", "", "", "", "", "", "", "", "v", ""],
+      ["", "", "", "", "", "", "", "", "", ""],
+      ["", "", "", "", "", "", "", "", "", ""],
+      ["", "^", "~", "~", "", "", "", "^", "", ""],
+      ["", "^", "", "~", "~", "", "", "", "", ""],
+      ["", "^", "", "", "~", "~", "", "", "", ""],
+    ];
+    
+const lightRow = row_num => {
+  const row = GRID[row_num - 1];
+  
+  return row;
+};
+
+console.log(lightRow(2));
