557dd60d5730491912060f4f7223ca2fa0dbd4cf
Kang Wang <kwang.inbox@gmail.com>
2018-10-13 10:07:45 -0700

    countColumns & countRows functions

---
+++ countColumns.js
@@ -0,0 +1,8 @@
+// ./countColumns.js
+const grid = require("./grid.js").GRID;
+
+const countColumns = () => grid[0].length;
+
+module.exports = {
+  countColumns
+};
---
+++ countRows.js
@@ -0,0 +1,8 @@
+// ./countRows.js
+const grid = require("./grid.js").GRID;
+
+const countRows = () => grid.length;
+
+module.exports = {
+  countRows
+};
