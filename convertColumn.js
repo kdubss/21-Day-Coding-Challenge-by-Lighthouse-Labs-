18169a7e43653f16ba8524b544cbf561fd70982d
Kang Wang <kwang.inbox@gmail.com>
2018-10-13 10:36:04 -0700

    Refactor to account for invalid column letters

--- convertColumn.js
+++ convertColumn.js
@@ -1,11 +1,14 @@
 // ./convertColumn.js
+const grid = require("./grid").GRID;
+const countColumns = require("./countColumns").countColumns;
 
 const convertColumn = coordinate => {
   const letters = "abcdefghijklmnopqrstuvwxyz".toUpperCase().split("");
   const coord_letter = coordinate[0].toUpperCase();
-  const col_index = letters.indexOf(coord_letter);
+  const letter_index = letters.indexOf(coord_letter);
+  const col_index = letter_index <= countColumns() ? letter_index : console.log(`Column ${coord_letter} is not a valid column in GRID!`);
   
-  return col_index;
+  return letter_index;
 };
 
 module.exports = {
