566c94c328c297ab5fffd6e43ca33b5dfd8ac04b
Kang Wang <kwang.inbox@gmail.com>
2018-10-13 11:50:58 -0700

    Refactor module

--- isCurrent.js
+++ isCurrent.js
@@ -1,7 +1,5 @@
 // ./isCurrent.js
 const GRID = require("./grid").GRID;
-const convertColumn = require("./convertColumn").convertColumn;
-const convertRow = require("./convertRow").convertRow;
 const lightCell = require("./lightCell").lightCell;
 
 const isCurrent = coord => {
--- isRock.js
+++ isRock.js
@@ -1,7 +1,5 @@
 // ./isRock.js
 const GRID = require("./grid").GRID;
-const convertColumn = require("./convertColumn").convertColumn;
-const convertRow = require("./convertRow").convertRow;
 const lightCell = require("./lightCell").lightCell;
 
 const isRock = coord => {
