f31d147c6d4b6bcd1463be6a9d35861c6e4b3b9d
Kang Wang <kwang.inbox@gmail.com>
2018-10-15 13:37:09 -0700

    Finish adding tests for all functions in 21 DCC so far

--- test/test.js
+++ test/test.js
@@ -11,6 +11,13 @@ const convertColumn = require("../functions/convertColumn").convertColumn;
 const convertRow = require("../functions/convertRow").convertRow;
 const lightCell = require("../functions/lightCell").lightCell;
 const isRock = require("../functions/isRock").isRock;
+const isCurrent = require("../functions/isCurrent").isCurrent;
+const isShip = require("../functions/isShip").isShip;
+const lightRow = require("../functions/lightRow").lightRow;
+const lightColumn = require("../functions/lightColumn").lightColumn;
+const allRocks = require("../functions/allRocks").allRocks;
+const allCurrents = require("../functions/allCurrents").allCurrents;
+const allShips = require("../functions/allShips").allShips;
 
 // countRows() function
 it("countRows() should return 10 as the length of GRID", () => {
@@ -50,4 +57,39 @@ it("lightCell(), when given a coordinate ('C2'), should return the contents of G
 // isRock function
 it("isRock(), when given a coordinate ('D1'), should return true or false if the cell contents of GRID is a rock ('^')", () => {
   assert(isRock("D1"), true);
+});
+
+// isCurrent function
+it("isCurrent(), when given a coordinate ('E2'), should return true or false if the cell contents of GRID is a current ('~')", () => {
+  assert(isCurrent("E2"), true);
+});
+
+// isShip function
+it("isShip(), when given a coordiante ('B3'), should return true or false if the cell contents of GRID is a ship ('V')", () => {
+  assert(isShip("B3"), true);
+})
+
+// lightRow function
+it("lightRow(), when given a row number ('1'), should return the entire contents of the corresponding GRID row (i.e. the array associated with that row in GRID)", () => {
+  assert(lightRow(2), ["", "", "v", "", "~", "", "", "", "", ""])
+});
+
+// lightColumn function
+it("lightColumn(), when given a column letter ('C'), should return an array of elements corresponding to that column in GRID", () => {
+  assert(lightColumn("C"), ["", "v", "", "", "", "", "", "~", "", ""])
+});
+
+// allRocks function
+it("allRocks(), when called, should return an array of coordinates of all GRID cells that contain rocks ('^')", () => {
+  assert(allRocks(), ["D1", "E3", "F3", "E4", "F4", "B8", "H8", "B9", "B10"]);
+});
+
+// allCurrents function
+it("allCurrents(), when called, should return an array of coordinates of all GRID cells that contain currents ('~')", () => {
+  assert(allCurrents(), ["E2", "C8", "D8", "D9", "E10", "F10"]);
+});
+
+// allShips function
+it("allShips(), when called, should return an array of coordinates of all GRID cdells that contain ships ('v')", () => {
+  assert(allShips(), ["C2", "B3", "I5"]);
 });
