2d32e7cffd13ab16560008f028c7b659dc3586cc
Kang Wang <kwang.inbox@gmail.com>
2018-10-16 10:16:48 -0700

    firstRock()

--- functions/firstRock.js
+++ functions/firstRock.js
@@ -3,38 +3,23 @@ const GRID = require("./grid").GRID;
 const countRows = require("./countRows").countRows;
 const countColumns = require("./countColumns").countColumns;
 
-const filterGrid = (filter_by) => {
-  const filtered_grid = [];
-  
-  for (let row = 0; row < countRows(); row++) {
-    filtered_grid.push(GRID[row].filter(by => {
-      return by === filter_by;
-    }));
-  }
-  
-  return filtered_grid;
-};
+const firstRock = () => {
+  const rock_coord = [];
+  const rock = "^";
 
-const getFirstRockInEachRow = () => {
-  const first_rock_col_no = [];
-  
   for (let row = 0; row < countRows(); row++) {
-    if (GRID[row].indexOf("^") !== -1) {
-      const rock_col_no = GRID[row].indexOf("^") + 1;
-      first_rock_col_no.push(rock_col_no);
+    for (let col = 0; col < countColumns(); col++) {
+      if (GRID[row][col] === rock) {
+        const coord = "abcdefghijklmnopqrstuvwxyz".toUpperCase().charAt(col) + (row + 1).toString();
+        rock_coord.push(coord);
+      }
     }
   }
-  
-  return first_rock_col_no;
-};
 
-const firstRock = () => {
-  return "First Rock!";
+  const first_rock = rock_coord[0];
+  return first_rock;
 };
 
 module.exports = {
-  getFirstRockInEachRow,
   firstRock
 };
-
-console.log(getFirstRockInEachRow());
