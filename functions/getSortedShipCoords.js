0b7edcd8495d50b355e981fb2076858474ec7b61
Kang Wang <kwang.inbox@gmail.com>
2018-10-18 10:48:05 -0700

    Rename function

--- functions/getSortedShipCoords.js
+++ functions/getSortedShipCoords.js
@@ -3,12 +3,12 @@ const GRID = require("./grid").GRID;
 const countColumns = require("./countColumns").countColumns;
 const countRows = require("./countRows").countRows;
 
-const getShipCoords = () => {
+const getSortedShipCoords = () => {
   let ship_coords = [];
   const ship = "v";
   
   for (let row = 0; row < countRows(); row++) {
-    for (let col = 0; col < countColumns(); col++) {
+    for (let col = 0; col < countColumns(); col ++) {
       if (GRID[row][col] === ship) {
         const coord = "abcdefghijklmnopqrstuvwxyz".toUpperCase().charAt(col) + (row + 1).toString();
         ship_coords.push(coord);
@@ -22,5 +22,5 @@ const getShipCoords = () => {
 };
 
 module.exports = {
-  getShipCoords
+  getSortedShipCoords
 };
