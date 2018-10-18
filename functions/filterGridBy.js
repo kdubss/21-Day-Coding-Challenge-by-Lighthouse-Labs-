f84e77f3417f973dcb5fdaabeed4f35a447a92cf
Kang Wang <kwang.inbox@gmail.com>
2018-10-18 11:54:13 -0700

    Implement function to filter GRID by criteria (i.e. rock, ship, etc.)

---
+++ functions/filterGridBy.js
@@ -0,0 +1,24 @@
+// ./functions/filterGridBy.js
+const GRID = require("./grid").GRID;
+const countRows = require("./countRows").countRows;
+const countColumns = require("./countColumns").countColumns;
+
+const filterGridBy = (fltr_by) => {
+  const filtered_array = [];
+  
+  for (let row = 0; row < countRows(); row++) {
+    for (let col = 0; col < countColumns(); col++) {
+      if (GRID[row][col] === fltr_by) {
+        const coord = "abcdefghijklmnopqrstuvwxyz".toUpperCase().charAt(col) + (row + 1).toString();
+        filtered_array.push(coord);
+      }
+    }
+  }
+  
+  return filtered_array;
+};
+
+const filter_by = ["^", "v", "~"];
+for (let row = 0; row < filter_by.length; row++) {
+  console.log(filterGridBy(filter_by[row]));
+}
