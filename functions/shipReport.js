a087b83cd575b2a2a79bff3f9681da2acf26aef5
Kang Wang <kwang.inbox@gmail.com>
2018-10-18 10:57:43 -0700

    shipReport module

---
+++ functions/shipReport.js
@@ -0,0 +1,19 @@
+// ./functions/shipReport.js
+const GRID = require("./grid").GRID;
+const countRows = require("./countRows").countRows;
+const countColumns = require("./countColumns").countColumns;
+const getSortedShipCoords = require("./getSortedShipCoords").getSortedShipCoords;
+
+const shipReport = () => {
+  const sorted_ship_coords = getSortedShipCoords();
+  const first_and_last_ship = [
+    sorted_ship_coords[0], 
+    sorted_ship_coords[sorted_ship_coords.length - 1]
+  ];
+  
+  return first_and_last_ship;
+};
+
+module.exports = {
+  shipReport
+};
