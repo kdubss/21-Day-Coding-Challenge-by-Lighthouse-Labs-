e77dd3f134486fe2007362d6de14d10bdf67d590
Kang Wang <kwang.inbox@gmail.com>
2018-10-18 10:11:28 -0700

    day17 challenge - getShipCoords

---
+++ functions/getShipCoords.js
@@ -0,0 +1,26 @@
+// ./functions/getShipCoords.js
+const GRID = require("./grid").GRID;
+const countColumns = require("./countColumns").countColumns;
+const countRows = require("./countRows").countRows;
+
+const getShipCoords = () => {
+  let ship_coords = [];
+  const ship = "v";
+  
+  for (let row = 0; row < countRows(); row++) {
+    for (let col = 0; col < countColumns(); col++) {
+      if (GRID[row][col] === ship) {
+        const coord = "abcdefghijklmnopqrstuvwxyz".toUpperCase().charAt(col) + (row + 1).toString();
+        ship_coords.push(coord);
+      }
+    }
+  }
+  
+  ship_coords = ship_coords.sort();
+  
+  return ship_coords;
+};
+
+module.exports = {
+  getShipCoords
+};
