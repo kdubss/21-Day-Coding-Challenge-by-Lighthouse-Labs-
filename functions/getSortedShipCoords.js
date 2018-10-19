2825d4c3921e5bad1e8c62f379b923be02b1a439
Kang Wang <kwang.inbox@gmail.com>
2018-10-18 19:59:11 -0700

    Add function docs

--- functions/getSortedShipCoords.js
+++ functions/getSortedShipCoords.js
@@ -3,7 +3,10 @@ const GRID = require("./grid").GRID;
 const countColumns = require("./countColumns").countColumns;
 const countRows = require("./countRows").countRows;
 const filterGridBy = require("./filterGridBy").filterGridBy;
-
+ /**
+  * [Returns an array of string-coordinates of all ships ("v") in GRID]
+  * @return {[String]} [An array with string coordinates ("B3")]
+  */
 const getSortedShipCoords = () => {
   const ship = "v";
   let ship_coords = filterGridBy(ship)
