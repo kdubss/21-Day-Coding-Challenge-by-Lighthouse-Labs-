d3a5189c60d25fba4ee1d65678654bcccedd5dd3
Kang Wang <kwang.inbox@gmail.com>
2018-10-20 11:29:26 -0700

    Refactor w/ new getPercentageOfInGrid()

--- functions/percentageReport.js
+++ functions/percentageReport.js
@@ -4,18 +4,17 @@ const countRows = require("./countRows").countRows;
 const countColumns = require("./countColumns").countColumns;
 const totalCells = require("./totalCells").totalCells;
 const filterGridBy = require("./filterGridBy").filterGridBy;
+const getPercentageOfInGrid = require("./getPercentageOfInGrid").getPercentageOfInGrid;
 
 /**
  * [percentageReport returns the proportion of rock and current grids within GRID]
  * @return {[Array]} [Array of proportions of occurrences, of rocks and currents, respectively]
  */
 const percentageReport = () => {
-  const rock = "^";
-  const current = "~";
-  const proportion_of_rocks = ((filterGridBy(rock).length / totalCells()) * 100).toFixed(2)
-  const proportion_of_currents = ((filterGridBy(current).length / totalCells()) * 100).toFixed(2);
+  const percentage_of_rocks = getPercentageOfInGrid("^");
+  const percentage_of_currents = getPercentageOfInGrid("~");
   
-  return [proportion_of_rocks, proportion_of_currents];
+  return [percentage_of_rocks, percentage_of_currents];
 };
 
 module.exports = {
