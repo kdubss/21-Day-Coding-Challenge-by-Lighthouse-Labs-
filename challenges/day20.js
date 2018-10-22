261f01c867705bad5677f892157e6a5362e6cf63
Kang Wang <kwang.inbox@gmail.com>
2018-10-21 20:41:42 -0700

    Refactored to not use other functions

--- challenges/day20.js
+++ challenges/day20.js
@@ -19,48 +19,17 @@ const GRID = [
 ];
 
 const safetyReport = () => {
-  const countRows = () => GRID.length;
-  const countColumns = () => GRID[0].length;
-  const convertRow = coord => {
-    const row_num = coord.slice(1);
-    const is_row_valid = row_num <= countRows();
-    const row_index = is_row_valid ? parseInt(row_num) - 1 : undefined;
-    
-    return row_index;
-  };
-  const convertColumn = coord => {
-    const letters = "abcdefghijklmnopqrstuvwxyz".toUpperCase().split("");
-    const coord_letter = coord[0].toUpperCase();
-    const letter_index = letters.indexOf(coord_letter);
-    const col_index = letter_index < countColumns() ? letter_index : undefined;
-    
-    return col_index;
-  };
-  const lightCell = coord => {
-    const row_index = convertRow(coord);
-    const col_index = convertColumn(coord);
-    const grid_cell = row_index < countRows() && col_index < countColumns() ? GRID[row_index][col_index] : undefined;
-    
-    return grid_cell;
-  };
-  const isRock = coord => lightCell(coord) === "^";
-  const isCurrent = coord => lightCell(coord) === "~";
-
   const grid = [];
   
-  for (let row = 0; row < countRows(); row++) {
-    grid[row] = [];
-    for (let col = 0; col < countColumns(); col++) {
-      const coord_letter = String.fromCharCode(col + 65);
-      const coord_number = (row + 1).toString();
-      const coord = coord_letter + coord_number;
-
-      if (lightCell(coord) === "^") {
-        grid[row].push(100);
-      } else if (lightCell(coord) === "~") {
-        grid[row].push(50);
-      } else if (lightCell(coord) === "") {
-        grid[row].push(0);
+  for (let row = 0; row < GRID.length; row++) {
+    grid[row] = []
+    for (let col = 0; col < GRID[0].length; col++) {
+      if (GRID[row][col] === "^") {
+        grid[row].push("100");
+      } else if (GRID[row][col] === "~") {
+        grid[row].push("50");
+      } else if (GRID[row][col] === "") {
+        grid[row].push("0");
       }
     }
   }
