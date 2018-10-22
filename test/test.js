8d97bbbbfe33bb15b3b01ee45dcae9c8c47527fb
Kang Wang <kwang.inbox@gmail.com>
2018-10-22 11:35:50 -0700

    WIP: test validations for day21 challenge functionality

--- test/test.js
+++ test/test.js
@@ -32,6 +32,8 @@ const shipReport = require("../functions/shipReport").shipReport;
 const filterGridBy = require("../functions/filterGridBy").filterGridBy;
 const getPercentageOfInGrid = require("../functions/getPercentageOfInGrid").getPercentageOfInGrid;
 const percentageReport = require("../functions/percentageReport").percentageReport;
+const getDistance4CellsOnSameRow = require("../functions/calcDistance").getDistance4CellsOnSameRow;
+const getDistance4getDistance4CellsOnSameColumn = require("../functions/calcDistance").getDistance4CellsOnSameColumn;
 
 describe("Testing GRID array", () => {
   it("GRID should be of type 'Array'", () => {
@@ -315,5 +317,31 @@ describe("Testing behavioural functionality of day 19 challenge", () => {
     it("should return an array [9.00, 7.00]", () => {
       assert.equal(percentageReport(), [9.00.toFixed(2), 7.00.toFixed(2)]);
     })
+  });
+});
+
+describe("Testing behavioural functionality of day 21 challenge", () => {
+  describe("getDistance4CellsOnSameRow() functrion", () => {
+    it("should be defined", () =>  {
+      assert.isDefined(getDistance4CellsOnSameRow);
+    });
+    it("should be a function", () => {
+      assert.isFunction(getDistance4CellsOnSameRow);
+    });
+    it("should return 9 as a distance", () => {
+      assert.equal(getDistance4CellsOnSameRow("A1", "J1"), 9);
+    })
+  });
+  
+  describe("getDistance4CellsOnSameColumn() function", () => {
+    it("should be defined", () => {
+      assert.isDefined(getDistance4getDistance4CellsOnSameColumn)
+    });
+    it("should be a function", () => {
+      assert.isFunction(getDistance4getDistance4CellsOnSameColumn);
+    });
+    it("should return 9 as a distance", () => {
+      assert.equal(getDistance4CellsOnSameColumn("B1", "B10"), 9);
+    });
   });
 });
