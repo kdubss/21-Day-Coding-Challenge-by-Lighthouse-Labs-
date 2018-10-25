c22d73309f732ab1025d407102e808bb1a336955
Kang Wang <kwang.inbox@gmail.com>
2018-10-25 15:27:23 -0700

    Refactor test for day 16 challenge - firstCurrent

--- test/day16_test.js
+++ test/day16_test.js
@@ -5,12 +5,22 @@ const should = chai.should;
 
 const GRID = require("../functions/grid").GRID;
 const firstCurrent = require("../functions/firstCurrent").firstCurrent;
+const filterGridBy = require("../functions/firstCurrent").firstCurrent;
 
 describe("Day 16 Challenge: firstCurrent()", () => {
-  it("should return a string as output", () => {
-    expect(firstCurrent()).to.be.a("string");
+  it("should be defined", () => {
+    assert.isDefined(firstCurrent);
   });
-  it("should return 'E2' as the GRID coordinate of the first current", () => {
-    assert.equal(firstCurrent(), "E2");
+  it("should be a function", () => {
+    assert.isFunction(firstCurrent);
+  });
+  it("should return a String as an output", () => {
+    assert.isString(firstCurrent());
+  });
+  it("should return a String value with length of 2", () => {
+    assert.lengthOf(firstCurrent(), 2);
+  });
+  it("should return a value of 'E2' as the GRID coord with the first current", () => {
+    expect(firstCurrent()).to.equal("E2");
   });
 });
