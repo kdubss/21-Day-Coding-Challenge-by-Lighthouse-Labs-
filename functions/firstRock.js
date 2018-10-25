e07508a36972e091f0b0bd612f9033dac9796add
Kang Wang <kwang.inbox@gmail.com>
2018-10-25 15:18:13 -0700

    Refactor function

--- functions/firstRock.js
+++ functions/firstRock.js
@@ -1,18 +1,11 @@
 // ./functions/firstRock.js
-const GRID = require("./grid").GRID;
 const filterGridBy = require("./filterGridBy").filterGridBy;
 
 /**
  * [firstRock returns GRID coordinate of first rock instance]
  * @return {[String]} [String coordinate ("D1") of GRID corresponding to first rock]
  */
-const firstRock = () => {
-  const rock = "^";
-  const rock_coords = filterGridBy(rock);
-  const first_rock = rock_coords[0];
-  
-  return first_rock;
-};
+ const firstRock = () => filterGridBy("^")[0];
 
 module.exports = {
   firstRock
