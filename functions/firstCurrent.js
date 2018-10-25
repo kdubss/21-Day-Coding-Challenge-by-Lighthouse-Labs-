ecdbb6b5dfc374bfadf88a8d9aa6fb115a38e161
Kang Wang <kwang.inbox@gmail.com>
2018-10-25 15:20:49 -0700

    Refactor

--- functions/firstCurrent.js
+++ functions/firstCurrent.js
@@ -1,18 +1,11 @@
 // ./functions/firstCurrent.js
-const GRID = require("./grid").GRID;
 const filterGridBy = require("./filterGridBy").filterGridBy;
 
 /**
  * [firstCurrent returns GRID coordinate of first current]
  * @return {[String]} [String coordinate ("E3") of GRID corresponding to first current]
  */
-const firstCurrent = () => {
-  const current = "~";
-  const current_coords = filterGridBy(current);
-  const first_current = current_coords[0]
-  
-  return first_current;
-};
+const firstCurrent = () => filterGridBy("~")[0];
 
 module.exports = {
   firstCurrent
